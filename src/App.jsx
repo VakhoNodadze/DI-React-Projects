55673455dc791039095b452f7095795398fcb165
VakhtangNodadze <vakhgang.nodadze@gmail.com>
2023-03-17 21:45:24 +0400

    form input and user render done

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,108 @@
 // @ts-nocheck
+import { useState } from "react";
 import "./App.scss";
-import TemperatureCard from "./components/Temperature";
 
 function App() {
-  // თუ ტემპერატურა 0ზე ნაკლებია, temperature დივის ბეგქრაუნდ ფერი იყოს ლურჯი, თუ მეტია 0ზე და ნაკლებია 15ზე იყოს მწვანე, 15დან 30მდე იყოს ყვითელი, ხოლო 30ის ზევით იყოს წითელი
+  // const [avatar, setAvatar] = useState("");
+  // const [firstName, setFirstName] = useState("");
+  // const [lastName, setLastName] = useState("");
+  const [user, setUser] = useState({
+    firstName: "",
+    lastName: "",
+    avatar: "",
+    phoneNumber: "",
+    city: "",
+  });
+  const [users, setUsers] = useState([]);
+
+  const handleValues = (target) =>
+    setUser((prev) => {
+      return { ...prev, [target.name]: target.value };
+    });
+
+  const handleAddUser = (user) => {
+    setUsers((prev) => [...prev, user]);
+    setUser({
+      firstName: "",
+      lastName: "",
+      avatar: "",
+      phoneNumber: "",
+      city: "",
+    });
+  };
 
   return (
-    <div style={{ width: "100%", display: "flex", flexWrap: "wrap" }}>
-      <TemperatureCard initialTemperature={0} />
-      <TemperatureCard initialTemperature={10} />
-      <TemperatureCard initialTemperature={-25} />
-    </div>
+    <>
+      <div>
+        <div className="input-container">
+          <label>Enter your Photo URL</label>
+          <input
+            name="avatar"
+            placeholder="...Photo"
+            value={user.avatar}
+            onChange={(event) => handleValues(event.target)}
+          />
+        </div>
+        <div className="input-container">
+          <label>Enter your First Name</label>
+          <input
+            placeholder="...John"
+            name="firstName"
+            value={user.firstName}
+            onChange={(e) => handleValues(e.target)}
+          />
+        </div>
+        <div className="input-container">
+          <label>Enter your Last Name</label>
+          <input
+            placeholder="...Doe"
+            name="lastName"
+            value={user.lastName}
+            onChange={(e) => handleValues(e.target)}
+          />
+        </div>
+        <div className="input-container">
+          <label>Enter your Phone number</label>
+          <input
+            placeholder="+995 555 123 456"
+            name="phoneNumber"
+            value={user.phoneNumber}
+            onChange={(e) => handleValues(e.target)}
+          />
+        </div>
+        <div className="input-container">
+          <label>Enter your City</label>
+          <input
+            placeholder="...Tbilisi"
+            name="city"
+            value={user.city}
+            onChange={(e) => handleValues(e.target)}
+          />
+        </div>
+        <button onClick={() => handleAddUser(user)}>Add user</button>
+      </div>
+      <div className="flex-wrap">
+        {users.map((user) => (
+          <div className="card">
+            <div className="card-header">
+              <img src={user.avatar} width="50px" height="100px" />
+              <p>
+                <strong>{user.firstName} </strong>{" "}
+                <strong> {user.lastName} </strong>
+              </p>
+            </div>
+            <div className="card-info">
+              <p>
+                Phone number: <strong>{user.phoneNumber}</strong>
+              </p>
+              <p>
+                City: <strong> {user.city} </strong>
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
 
